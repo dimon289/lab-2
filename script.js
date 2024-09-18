743a3b69ff18d1c2c4bacc377ab14dedba8da3ee
dima tkachenko <dtkachenko06@gmail.com>
2024-09-18 17:50:50 +0300

    compleate task 3-4

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 const year = 2024;
 
-const func = (name, yearOfBirth = 2000) =>{
-    return `Я ${name}, мені ${year-yearOfBirth} років`
-}
-
-console.log(func("Дмитро", 2006))
-console.log(func("Дмитро"))
-
-
-// class Human {
-//     constructor(name, age, weight, gender) {
-//         this.name = name;
-//         this.age = age;
-//         this.weight = weight;
-//         this.gender = gender
-//     }
-//     grtInfo(prop){
-//         return prop ? prop == null:{name:this.name,age:this.age,weight:this.weight,gender:this.gender};
-//     }
-//     greeeting(){}
+// const func = (name, yearOfBirth = 2000) =>{
+//     return `Я ${name}, мені ${year-yearOfBirth} років`
 // }
 
-// let Dima = new Human("Дмитро", 18, "80kg", "man")
+// console.log(func("Дмитро", 2006))
+// console.log(func("Дмитро"))
 
-// console.log(Dima.grtInfo(null))
+
+class Human {
+    constructor(name, age, weight, gender) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.gender = gender
+    }
+    grtInfo(prop){
+        return prop == null ?{name:this.name, age:this.age, weight:this.weight, gender:this.gender}: this[prop];
+    }
+    greeeting(){
+        return "Hi!"
+    }
+}
+
+let Dima = new Human("Дмитро", 18, "80kg", "man")
+
+console.log(Dima.grtInfo(null))
+console.log(Dima.grtInfo("name"))
+console.log(Dima.greeeting())
+
