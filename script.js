e538c0369dab1abd58f454823635167717a6dbf6
dima tkachenko <dtkachenko06@gmail.com>
2024-09-19 16:26:49 +0300

    compleate task 6-7

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ class Human {
         this.weight = weight;
         this.gender = gender
     }
-    grtInfo(prop){
+    getInfo(prop){
         return prop == null ?{name:this.name, age:this.age, weight:this.weight, gender:this.gender}: this[prop];
     }
     greeeting(){
@@ -43,6 +43,7 @@ class Woman extends Human{
 
 
 
+
 // let Dima = new Human("Дмитро", 18, "80kg", "man")
 
 // console.log(Dima.grtInfo(null))
@@ -50,7 +51,53 @@ class Woman extends Human{
 // console.log(Dima.greeeting())
 
 
-let Illa = new Man("Ілля", 19, "55kg");
-let Ulianna = new Woman("Юліанна", 18, "60kg");
-console.log(Illa.greeeting());
-console.log(Ulianna.greeeting());
+// let Illa = new Man("Ілля", 19, "55kg");
+// let Ulianna = new Woman("Юліанна", 18, "60kg");
+// console.log(Illa.greeeting());
+// console.log(Ulianna.greeeting());
+
+
+
+let list = [
+    new Man("Dima", 18, "98kg"),
+    new Man("Oleg", 30, "157kg"),
+    new Man("Illa", 19, "63kg"),
+    new Woman("Jullia", 25, "75kg"),
+    new Man("Bogdan", 44, "93kg"),
+    new Woman("Natalia", 35, "88kg"),
+    new Man("Rostic", 25, "94kg"),
+    new Woman("Sasha", 20, "56kg"),
+    new Woman("Anya", 19, "80kg"),
+    new Woman("Polina", 14, "52kg"),
+    new Man("Artem", 14, "61kg"),
+    new Man("Oleksandr", 33, "95kg"),
+    new Woman("Katerina", 27, "91kg"),
+    new Man("Dmitro", 38, "82kg"),
+    new Woman("Jullia", 22, "119kg"),
+];
+
+
+const search = (list, searchField, searchValue) =>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(() =>{
+            if(list.some((item) => item[searchField] === searchValue)){
+                resolve(list.filter((item) => item[searchField] === searchValue));
+            }
+            else{
+                reject((`таких елементів за пошуком ${searchField}:${searchValue} не знайдено`))
+            }
+        }, executingTime = 300);
+    });
+}
+
+
+
+search(list, "name", "Jullia")
+    .then(e => e.forEach(element => console.log(element.getInfo(null))))
+    .catch(r => console.log(r))
+
+search(list, "nam", "Jullia")
+    .then(e => e.forEach(element => console.log(element.getInfo(null))))
+    .catch(r => console.log(r))
+
+
